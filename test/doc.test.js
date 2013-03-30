a7d49029ba95f2df9ab57ac2a3c4ea4b9e0267de
Tim Shadel <tim@shadelsoftware.com>
2013-03-29 20:51:30 -0600

    Try out a connect-style approach.

---
+++ test/doc.test.js
@@ -0,0 +1,34 @@
+// var doc = require('../lib/doc')
+//   , http = require('./fake-http')
+//   , user = http.get(0)
+//   , reg = http.get(1);
+
+// var document = doc(user, 'http://facebook.com/bhavesh', 'http://facebook.com/employers/acme');
+// document.root.employer.open();
+// document().employer.open();
+
+// document = doc(reg, 'http://37s.com/register', 'http://google.com/search?q=register');
+// document().register().email("samuel@example.com");
+// console.log(document().register().email());
+// console.log(document.root.register.form.email());
+// document().register.submit();
+
+
+/////////////////////
+
+// hyper(reg)
+//   .register
+//   .name("Sam")
+//   .email("samuel@example.com")
+//   .submit();
+
+// var form = hyper(reg)
+//   .register
+//   .name("Sam")
+//   .email("samuel@example.com");
+
+// // TODO
+// // form.data.register.action = '/update';
+// // form.rewire();
+// // form.submit();
+
