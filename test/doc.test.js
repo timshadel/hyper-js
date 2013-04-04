b797f4cc0605f21f980e1d35dd078b043aac4151
Tim Shadel <github@timshadel.com>
2013-04-04 16:27:31 -0600

    Clean up test structure.

--- test/doc.test.js
+++ test/doc.test.js
@@ -1,52 +1,67 @@
+/**
+ * Test dependencies.
+ */
 
 var hyper = require('../')
   , expect = require('expect.js');
 
-describe('hyper.doc', function(){
-  describe('should throw an exception', function(){
-    it('on numbers', function(done){
-      expect(function(){ hyper.doc(6); }).to.throwException(/invalid/i);
-      done();
+
+/**
+ * Test `doc` inputs.
+ */
+
+function docWith(arg) {
+  return function() {
+    hyper.doc(arg);
+  };
+}
+
+describe('hyper.doc()', function(){
+
+  describe('should accept', function(){
+    it('plain objects', function(){
+      expect(hyper.doc({})).to.be.a(hyper.HyperDocument);
     });
-    it('on regex', function(done){
-      expect(function(){ hyper.doc(/bob/); }).to.throwException(/invalid/i);
-      done();
-    });
-    it('on undefined', function(done){
-      expect(function(){ hyper.doc(undefined); }).to.throwException(/invalid/i);
-      done();
-    });
-    it('on boolean', function(done){
-      expect(function(){ hyper.doc(true); }).to.throwException(/invalid/i);
-      done();
-    });
-    it('on null', function(done){
-      expect(function(){ hyper.doc(null); }).to.throwException(/invalid/i);
-      done();
-    });
-    // Strings must be valid JSON
-    it('on invalid JSON strings', function(done){
-      expect(function(){ hyper.doc('I am not valid JSON.'); }).to.throwException(/valid json/i);
-      done();
-    });
-    it('on empty strings', function(done){
-      expect(function(){ hyper.doc(''); }).to.throwException(/invalid/i);
-      done();
-    });
-    // JSON may only contain a single top-level object (really??)
-    it('on valid JSON strings with top-level arrays', function(done){
-      expect(function(){ hyper.doc([]); }).to.throwException(/array/i);
-      done();
+    it('valid JSON strings of a single object', function(){
+      expect(hyper.doc('{}')).to.be.a(hyper.HyperDocument);
     });
   });
 
-  describe('given a valid object', function(){
-    describe('which has no hyper features', function(){
-      it('should return a hyper doc', function(done){
-        var doc = hyper.doc('{}');
-        expect(doc).to.be.a(hyper.HyperDocument);
-        done();
-      });
+  // Test variants of invalid inputs
+  describe('should not accept', function(){
+    it('numbers', function(){
+      expect(docWith(6)).to.throwException(/invalid/i);
+      expect(docWith('6')).to.throwException(/invalid/i);
+    });
+    it.skip('regex', function(){
+      expect(docWith(/bob/)).to.throwException(/invalid/i);
+      expect(docWith('/bob/')).to.throwException(/invalid/i);
+    });
+    it('undefined', function(){
+      expect(docWith(undefined)).to.throwException(/invalid/i);
+      expect(docWith('undefined')).to.throwException(/invalid/i);
+    });
+    it('boolean', function(){
+      expect(docWith(true)).to.throwException(/invalid/i);
+      expect(docWith('true')).to.throwException(/invalid/i);
+      expect(docWith(false)).to.throwException(/invalid/i);
+      expect(docWith('false')).to.throwException(/invalid/i);
+    });
+    it('null', function(){
+      expect(docWith(null)).to.throwException(/invalid/i);
+      expect(docWith('null')).to.throwException(/invalid/i);
+    });
+    // Strings must be valid JSON
+    it('invalid JSON strings', function(){
+      expect(docWith('I am not valid JSON.')).to.throwException(/valid json/i);
+    });
+    it('empty strings', function(){
+      expect(docWith('')).to.throwException(/invalid/i);
+    });
+    // JSON may only contain a single top-level object (really??)
+    it('valid JSON strings with top-level arrays', function(){
+      expect(docWith([])).to.throwException(/array/i);
+      expect(docWith('[]')).to.throwException(/array/i);
     });
   });
 
