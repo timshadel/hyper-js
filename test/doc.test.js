f2522d0e11976609229a9a41be0e554cd82c468f
Tim Shadel <github@timshadel.com>
2013-04-04 17:28:06 -0600

    Narrow possible exceptions.

--- test/doc.test.js
+++ test/doc.test.js
@@ -30,33 +30,33 @@ describe('hyper.doc()', function(){
   // Test variants of invalid inputs
   describe('should not accept', function(){
     it('numbers', function(){
-      expect(doc(6)).to.throwException(/invalid/i);
-      expect(doc('6')).to.throwException(/invalid/i);
+      expect(doc(6)).to.throwException(/invalid input/i);
+      expect(doc('6')).to.throwException(/invalid input/i);
     });
     it.skip('regex', function(){
-      expect(doc(/bob/)).to.throwException(/invalid/i);
-      expect(doc('/bob/')).to.throwException(/invalid/i);
+      expect(doc(/bob/)).to.throwException(/invalid input/i);
+      expect(doc('/bob/')).to.throwException(/invalid input/i);
     });
     it('undefined', function(){
-      expect(doc(undefined)).to.throwException(/invalid/i);
-      expect(doc('undefined')).to.throwException(/invalid/i);
+      expect(doc(undefined)).to.throwException(/invalid input/i);
+      expect(doc('undefined')).to.throwException(/invalid input/i);
     });
     it('boolean', function(){
-      expect(doc(true)).to.throwException(/invalid/i);
-      expect(doc('true')).to.throwException(/invalid/i);
-      expect(doc(false)).to.throwException(/invalid/i);
-      expect(doc('false')).to.throwException(/invalid/i);
+      expect(doc(true)).to.throwException(/invalid input/i);
+      expect(doc('true')).to.throwException(/invalid input/i);
+      expect(doc(false)).to.throwException(/invalid input/i);
+      expect(doc('false')).to.throwException(/invalid input/i);
     });
     it('null', function(){
-      expect(doc(null)).to.throwException(/invalid/i);
-      expect(doc('null')).to.throwException(/invalid/i);
+      expect(doc(null)).to.throwException(/invalid input/i);
+      expect(doc('null')).to.throwException(/invalid input/i);
     });
     // Strings must be valid JSON
     it('invalid JSON strings', function(){
-      expect(doc('I am not valid JSON.')).to.throwException(/valid json/i);
+      expect(doc('I am not valid JSON.')).to.throwException(/invalid json/i);
     });
     it('empty strings', function(){
-      expect(doc('')).to.throwException(/invalid/i);
+      expect(doc('')).to.throwException(/invalid input/i);
     });
     // JSON may only contain a single top-level object (really??)
     it('valid JSON strings with top-level arrays', function(){
