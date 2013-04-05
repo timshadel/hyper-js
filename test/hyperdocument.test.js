4763bca6ae553cbf68b9a90d2eecd154bddc9c89
Tim Shadel <github@timshadel.com>
2013-04-05 13:26:49 -0600

    Tests showing raw vs doc.

--- test/hyperdocument.test.js
+++ test/hyperdocument.test.js
@@ -21,10 +21,28 @@ describe('HyperDocument', function(){
     });
 
     it('should make the original object under the `data` key', function(){
-      var doc = new HyperDocument({ "href": "something" }, 'http://example.com/resource');
-      expect(doc.data).to.be.ok();
-      expect(doc.href).to.not.be.ok();
-      expect(doc.data.href).to.equal("something");
+      var raw = { "href": "something" };
+      var equivalent = { "href": "something" };
+      var doc = new HyperDocument(raw, 'http://example.com/resource');
+      expect(doc.data).to.be(raw);
+      expect(doc.data).to.not.be(equivalent);
+      expect(doc.data).to.eql(equivalent);
+    });
+
+    it('should change if the raw object is changed', function(done){
+      var raw = { "href": "something" };
+      var doc = new HyperDocument(raw, 'http://example.com/resource');
+      doc.on('open', function(href) {
+        expect(href).to.equal('http://example.com/something');
+        raw.href = "another-item";
+        doc.removeAllListeners('open');
+        doc.on('open', function(newref) {
+          expect(newref).to.equal('http://example.com/another-item');
+          done();
+        });
+        doc.open();
+      });
+      doc.open();
     });
 
     it('valid JSON strings of a single object', function(){
